403ad7b94bccdffe43d944d32155557c8ceb20f4
shamzaou <cosmosoupro@gmail.com>
2021-09-19 17:41:08 +0100

    :heavy_check_mark: ft_atoi done

---
+++ C04/ex03/ft_atoi.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mail <shamzaou@student.42abudhabi.ae>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/07 11:48:19 by shamzaou          #+#    #+#             */
+/*   Updated: 2021/06/07 11:48:41 by shamzaou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_atoi(char *str)
+{
+	int n;
+	int negative;
+
+	while ((*str >= 9 && *str <= 13) || *str == ' ')
+		str++;
+	negative = 0;
+	while (*str == '-' || *str == '+')
+		if (*str++ == '-')
+			negative = 1 - negative;
+	n = 0;
+	while (*str >= '0' && *str <= '9')
+		n = n * 10 + *str++ - '0';
+	if (negative)
+		n *= -1;
+	return (n);
+}
