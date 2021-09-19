729d622d18ce01766c3c530f8139f64481126716
shamzaou <cosmosoupro@gmail.com>
2021-09-19 17:51:45 +0100

    :heavy_check_mark: ft_rev_params done

---
+++ C06/ex02/ft_rev_params.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_params.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mail <shamzaou@student.42abudhabi.ae>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/07 12:23:23 by shamzaou          #+#    #+#             */
+/*   Updated: 2021/06/07 12:25:20 by shamzaou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int		main(int argc, char *argv[])
+{
+	int i;
+	
+	i = argc - 1;
+	while(i >= 1)
+	{
+		ft_putstr(argv[i]);
+		write(1, "\n", 1);
+		i--;
+	}
+	return (0);
+}
+
