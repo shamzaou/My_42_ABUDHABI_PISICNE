6c924316857b76990384062cb52adf02a623356c
shamzaou <cosmosoupro@gmail.com>
2021-09-19 17:29:13 +0100

    :heavy_check_mark: ft_strncat done

---
+++ C03/ex03/ft_strncat.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mail <shamzaou@student.42abudhabi.ae>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/02 06:19:15 by shamzaou          #+#    #+#             */
+/*   Updated: 2021/06/02 06:47:23 by shamzaou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	unsigned int	i1;
+	unsigned int	i2;
+
+	i1 = 0;
+	i2 = 0;
+	while (dest[i1] != '\0')
+		i1++;
+	while (src[i2] != '\0' && i2 < nb)
+	{
+		dest[i1] = src[i2];
+		i1++;
+		i2++;
+	}
+	dest[i1] = '\0';
+	return (dest);
+}
+
