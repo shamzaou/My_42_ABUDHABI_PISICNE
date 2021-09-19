f2279c40ff48ae3325a2707ecb0e3cd704f60162
shamzaou <cosmosoupro@gmail.com>
2021-09-19 17:29:40 +0100

    :heavy_check_mark: ft_strstr done

---
+++ C03/ex04/ft_strstr.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mail <shamzaou@student.42abudhabi.ae>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/02 06:48:16 by shamzaou          #+#    #+#             */
+/*   Updated: 2021/06/02 06:49:15 by shamzaou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+char	*ft_strstr(char *str, char *to_find)
+{
+	int i;
+	int tmp;
+
+	if (*to_find == '\0')
+		return (str);
+	while (*str != '\0')
+	{
+		if (*str == *to_find)
+		{
+			i = 0;
+			tmp = 0;
+			while (to_find[i] != '\0')
+			{
+				if (str[i] != to_find[i])
+					tmp = 1;
+				i++;
+			}
+			if (tmp == 0)
+				return (str);
+		}
+		str++;
+	}
+	return (0);
+}
+
